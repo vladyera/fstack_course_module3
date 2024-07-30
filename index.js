5296dbfcc0b3e85b9e3ac8b4aad6a344f1e7dd7b
Uladzislau Yerashevich <74885175+vladyera@users.noreply.github.com>
2024-07-29 23:15:19 -0700

    Most of the linter fixes

--- index.js
+++ index.js
@@ -1,157 +1,129 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
-const Person = require('./models/person');
-
-// Phonebook default
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
 // CORS
-app.use(cors());
+app.use(cors())
 // For static website
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 // Adding JSON parser
-app.use(express.json());
+app.use(express.json())
 // Logger
-morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '');
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 // GET
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    response.json(persons);
+    response.json(persons)
   })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.get('/api/info', (request, response) => {
   Person.find({}).then(persons => {
-    const numberOfPersons = persons.length;
-    const currentTime = new Date();
-    currentTime.setHours(currentTime.getHours() + 2);
-    const currentTimeGMTPlus2 = currentTime.toISOString().replace(/T/, ' ').replace(/\..+/, '') + " GMT+2";
+    const numberOfPersons = persons.length
+    const currentTime = new Date()
+    currentTime.setHours(currentTime.getHours() + 2)
+    const currentTimeGMTPlus2 = currentTime.toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' GMT+2'
     const htmlContent = `
     <p>Phonebook has info for ${numberOfPersons} people</p>
     <p>${currentTimeGMTPlus2} (Eastern European Standard Time)</p>
-  `;
-    response.send(htmlContent);
+  `
+    response.send(htmlContent)
   })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 // DELETE
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
-    .catch(error => next(error));
-});
-
-const generateId = () => {
-  const newId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-  return newId;
-};
+    .catch(error => next(error))
+})
 
 // POST
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number is missing'
-    });
+    })
   }
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
   person.save()
     .then(savedPerson => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 // PUT
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number is missing'
-    });
+    })
   }
 
   const person = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       if (updatedPerson) {
-        response.json(updatedPerson);
+        response.json(updatedPerson)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 // Error handlers in middleware
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-app.use(unknownEndpoint);
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error('Error handler triggered')
+  console.error(error.message)
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
-  next(error);
-};
-app.use(errorHandler);
+  next(error)
+}
+app.use(errorHandler)
 
 // Listen
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Setting up
-mongoose.set('strictQuery', false);
-const url = process.env.MONGODB_URI;
+mongoose.set('strictQuery', false)
+const url = process.env.MONGODB_URI
 mongoose.connect(url)
-  .then(result => {
-    console.log('Connected to MongoDB');
+  .then(() => {
+    console.log('Connected to MongoDB')
   })
   .catch(error => {
-    console.error('Error connecting to MongoDB ', error);
+    console.error('Error connecting to MongoDB ', error)
   })
 
 const personSchema = new mongoose.Schema({
@@ -22,19 +22,19 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function(v) {
-        return /^(\d{2,3})-\d{5,}$/.test(v);
+        return /^(\d{2,3})-\d{5,}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     }
   },
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
